d449dbb54bfd5fda137d85b0edb76f6eecdd2ab0
Przemek Kaleta <przemyslawkaleta@gmail.com>
2021-02-22 21:17:20 +0100

    jsoup added it can takes titles from articles at websites, nice and easy

--- src/chemicalboy/base/Main.java
+++ src/chemicalboy/base/Main.java
@@ -1,8 +1,31 @@
 package chemicalboy.base;
 
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Connection connect = Jsoup.connect("http://www.onet.pl/");
+
+        try{
+            Document document = connect.get();
+
+            Elements links = document.select("span.title");
+            for(Element elem : links){
+                System.out.println(elem.text());
+            }
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
